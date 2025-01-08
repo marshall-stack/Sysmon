f6b0f9042dcd354444eaf78d5483d6e28dd1b692
theideaenthusiast <78301540+marshall-stack@users.noreply.github.com>
2025-01-08 11:19:54 +0100

    run.py

--- run.py
+++ run.py
@@ -9,26 +9,10 @@ def index():
 
 @app.route('/api/stats')
 def stats():
-    # Get CPU temperatures
-    try:
-        temps = psutil.sensors_temperatures()
-        cpu_temp = temps['coretemp'][0].current if 'coretemp' in temps else None
-    except AttributeError:
-        cpu_temp = None
-
-    # Get fan speeds
-    try:
-        fans = psutil.sensors_fans()
-        fan_speed = fans['fan'][0].current if 'fan' in fans else None
-    except AttributeError:
-        fan_speed = None
-
     data = {
-        "cpu": psutil.cpu_percent(interval=1, percpu=True),
+        "cpu": psutil.cpu_percent(interval=1),
         "memory": psutil.virtual_memory().percent,
-        "disk": psutil.disk_usage('/').percent,
-        "cpu_temp": cpu_temp,
-        "fan_speed": fan_speed,
+        "disk": psutil.disk_usage('/').percent
     }
     return jsonify(data)
 
